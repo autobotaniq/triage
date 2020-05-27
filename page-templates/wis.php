fb96828d4965972402daadb373d92160bc563183
Denis Lipatov <ldu2601@gmail.com>
2020-05-27 17:28:23 +0300

    27.05.2020 17:28 fix

--- page-templates/wis.php
+++ page-templates/wis.php
@@ -20,11 +20,21 @@ $tg_software_solution_1_img = get_field('tg_software_solution_1_img');
 <div class="tg-panel-solution">
 
   <div class="tg-panel-solution__top">
-    <div class="d-flex flex-wrap justify-content-between">
+    <div class="d-flex flex-wrap justify-content-between align-items-start">
       <div class="tg-panel-solution__text">
-
+        <div class="brand-primary-color f-size-lg-22 f-size-18 text-uppercase">
+          <?php echo $tg_software_solution_1_text['beforetitle'] ?>
+        </div>
+        <div class="f-size-lg-36 f-size-md-30 f-size-26 f-weight-700 l-height-110 py-10">
+          <?php echo $tg_software_solution_1_text['title'] ?>
+        </div>
+        <div class="f-size-lg-20 f-size-18">
+          <?php echo $tg_software_solution_1_text['subtitle'] ?>
+        </div>
+        <div class="f-size-lg-18 f-size-16 f-weight-300 pt-10 l-height-120">
+          <?php echo $tg_software_solution_1_text['description'] ?>
+        </div>
       </div>
-
 
       <div class="tg-panel-solution__jp">
         <div class="tg-panel-solution__jp-img">
@@ -36,6 +46,21 @@ $tg_software_solution_1_img = get_field('tg_software_solution_1_img');
     </div>
   </div>
 
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-xl-10">
+
+        <div class="row">
+          <div class="col-lg-6 py-15">
+            <div class="tg-panel-solution__item p-md-30 p-20">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 
 </div>
 
