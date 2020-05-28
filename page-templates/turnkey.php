ebd6dcf4ffef13aa8d8c56794346af1c21676aee
aerb20 <n0gep1990@gmail.com>
2020-05-28 22:16:57 +0300

    am turnkey v1

---
+++ page-templates/turnkey.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ * Template Name: Turkney Solution
+ */
+
+// Do not allow directly accessing this file.
+if (!defined('ABSPATH')) {
+	exit('Direct script access denied.');
+}
+get_header();
+?>
+<div class="tg-turnkey">
+	<div class="tg-turnkey__section-1">
+		<div class="brand-secondary-bg tg-turnkey__section-1__block">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="tg-turnkey__section-1__block__content mt-60 py-10 px-20 col-xl-9">
+						<div class="part-of-border brand-primary-bg"></div>
+						<div class="main-block">
+							<h2 class="pt-30 f-size-lg-42 f-size-md-36 f-size-30 mb-0 l-height-100">Planning & Design Services</h2>
+							<h3 class="pt-5 f-size-18">Optimize. Maximize. Deliver.</h3>
+							<p class="f-size-lg-18 f-size-md-16 f-size-15 l-height-130 mb-0 pb-20">Unmatched expertise in technology, business process optimization, and workflow management. Triage Partners offers the strategic advice, trusted insights, and practical tools that help to successfully develop and implement customer solutions to maximize workflow efficiency, scalability, and quality.</p>
+						</div>
+						<div class="lower-block">
+							<h4 class="f-size-lg-18 f-size-md-16 f-size-15 mb-5 pr-15">Our consulting services include:</h4>
+							<ul class="brand-primary-bg py-10 pl-15 ml-0 f-size-md-16 f-size-15">
+								<li>Material Flow Analysis & Optimization</li>
+								<li>Inventory Turns and Timeline Assessment & Enhancement</li>
+								<li>Cost of Product Evaluation & Optimization</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="tg-turnkey__section-1__block">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="tg-turnkey__section-1__block__content mt-60 py-10 px-20 col-xl-9">
+                        <div class="part-of-border brand-primary-bg"></div>
+                        <div class="main-block">
+                            <h2 class="pt-30 f-size-lg-42 f-size-md-36 f-size-30 mb-0 l-height-100">Planning & Design Services</h2>
+                            <h3 class="pt-5 f-size-18">Optimize. Maximize. Deliver.</h3>
+                            <p class="f-size-lg-18 f-size-md-16 f-size-15 l-height-130 mb-0 pb-20">Unmatched expertise in technology, business process optimization, and workflow management. Triage Partners offers the strategic advice, trusted insights, and practical tools that help to successfully develop and implement customer solutions to maximize workflow efficiency, scalability, and quality.</p>
+                        </div>
+                        <div class="lower-block">
+                            <h4 class="f-size-lg-18 f-size-md-16 f-size-15 mb-5 pr-15">Our consulting services include:</h4>
+                            <ul class="brand-primary-bg py-10 pl-15 ml-0 f-size-md-16 f-size-15">
+                                <p>Our candidates do more than look good on paper. We hire experienced resources and ensure their qualifications through technical testing, behavioral evaluation, and detailed background checks. </p>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+    <div class="tg-turnkey__section-2">
+        <div class="tg-explore py-40 pb-80">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-10 col-lg-11">
+                        <div class="tg-title text-uppercase f-size-lg-36 f-size-md-30 f-size-24 l-height-130 f-weight-700 mb-20">
+                            SEE TRIAGE partners AT WORK
+                        </div>
+                        <div class="row justify-content-around mt-40">
+                            <div class="py-15">
+                                <div class="tg-explore__item">
+                                    <div class="tg-explore__icon">
+                                        <div class="tg-explore__icon-img" style="background-image:url('<?php bloginfo('template_url'); ?>/assets/img/tele_design.png');"></div>
+                                        <span class="f-size-lg-22 f-size-md-20 f-size-18 f-weight-500 l-height-120">Telecom Design & Engineering </span>
+                                    </div>
+                                    <div class="rd-more py-10 pl-10 f-weight-700 f-size-16"><a href="#">Read More</a></div>
+                                </div>
+                            </div>
+                            <div class="py-15">
+                                <div class="tg-explore__item">
+                                    <div class="tg-explore__icon">
+                                        <div class="tg-explore__icon-img" style="background-image:url('<?php bloginfo('template_url'); ?>/assets/img/tele_design.png');"></div>
+                                        <span class="f-size-lg-22 f-size-md-20 f-size-18 f-weight-500 l-height-120">Telecom Design & Engineering </span>
+                                    </div>
+                                    <div class="rd-more py-10 pl-10 f-weight-700 f-size-16"><a href="#">Read More</a></div>
+                                </div>
+                            </div>
+                            <div class="py-15">
+                                <div class="tg-explore__item">
+                                    <div class="tg-explore__icon">
+                                        <div class="tg-explore__icon-img" style="background-image:url('<?php bloginfo('template_url'); ?>/assets/img/tele_design.png');"></div>
+                                        <span class="f-size-lg-22 f-size-md-20 f-size-18 f-weight-500 l-height-120">Telecom Design & Engineering </span>
+                                    </div>
+                                    <div class="rd-more py-10 pl-10 f-weight-700 f-size-16"><a href="#">Read More</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tg-turnkey__section-3 brand-secondary-bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-10 py-60">
+                    <div class="tg-title text-uppercase f-size-lg-36 f-size-md-30 f-size-24 l-height-130 f-weight-700 mb-20">
+                        WHY TRIAGE partners?
+                    </div>
+                    <div class="tg-turnkey__section-3__main-content">
+                        <div class="feature">
+                            <h4 class="f-size-md-20 f-size-18">Every engagement is unique. </h4>
+                            <p class="f-size-md-16 f-size-14">We can help you develop a solid strategy that builds the best workforce possible and the best solutions, based on your budget.</p>
+                        </div>
+                        <div class="feature">
+                            <h4 class="f-size-md-20 f-size-18">From workflows to workforce, we do it all. </h4>
+                            <p class="f-size-md-16 f-size-14">With our combination of industry experts and best practices backed by more than 100 years of combined experience, we can help your organization reach its full potential.</p>
+                        </div>
+                        <div class="feature">
+                            <h4 class="f-size-md-20 f-size-18">Every engagement is unique. </h4>
+                            <p class="f-size-md-16 f-size-14">We can help you develop a solid strategy that builds the best workforce possible and the best solutions, based on your budget.</p>
+                        </div>
+                        <div class="feature">
+                            <h4 class="f-size-md-20 f-size-18">Every engagement is unique. </h4>
+                            <p class="f-size-md-16 f-size-14">We can help you develop a solid strategy that builds the best workforce possible and the best solutions, based on your budget.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<?php
+	//$tg_template_art_title = get_field('tg_software_art_title');
+
+	$tg_template_art_reverse = false;
+
+	include(locate_template('template-parts/template-art.php', false, false));
+	?>
+</div>
+<?php get_footer(); ?>
