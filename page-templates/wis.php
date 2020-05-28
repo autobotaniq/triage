cabdf21fd8512cad9c3541bdf61daa1a2c6ecf5f
Denis Lipatov <ldu2601@gmail.com>
2020-05-28 10:44:01 +0300

    28.05.2020 10:43

--- page-templates/wis.php
+++ page-templates/wis.php
@@ -16,52 +16,138 @@ get_header();
 <?php
 $tg_software_solution_1_text = get_field('tg_software_solution_1_text');
 $tg_software_solution_1_img = get_field('tg_software_solution_1_img');
+$tg_software_solution_1_blocks = get_field('tg_software_solution_1_blocks');
 ?>
-<div class="tg-panel-solution">
+  <div class="tg-panel-solution pb-lg-60 pb-35">
 
-  <div class="tg-panel-solution__top">
-    <div class="d-flex flex-wrap justify-content-between align-items-start">
-      <div class="tg-panel-solution__text">
-        <div class="brand-primary-color f-size-lg-22 f-size-18 text-uppercase">
-          <?php echo $tg_software_solution_1_text['beforetitle'] ?>
-        </div>
-        <div class="f-size-lg-36 f-size-md-30 f-size-26 f-weight-700 l-height-110 py-10">
-          <?php echo $tg_software_solution_1_text['title'] ?>
-        </div>
-        <div class="f-size-lg-20 f-size-18">
-          <?php echo $tg_software_solution_1_text['subtitle'] ?>
-        </div>
-        <div class="f-size-lg-18 f-size-16 f-weight-300 pt-10 l-height-120">
-          <?php echo $tg_software_solution_1_text['description'] ?>
-        </div>
-      </div>
-
-      <div class="tg-panel-solution__jp">
-        <div class="tg-panel-solution__jp-img">
-          <img src="<?php echo esc_url($tg_software_solution_1_img['sizes']['info_block_one']) ?>"
-               alt="">
-        </div>
-        <div class="tg-panel-solution__jp-layout"></div>
-      </div>
-    </div>
-  </div>
-
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-xl-10">
-
-        <div class="row">
-          <div class="col-lg-6 py-15">
-            <div class="tg-panel-solution__item p-md-30 p-20">
-
-            </div>
+    <div class="tg-panel-solution__top">
+      <div class="d-flex flex-wrap justify-content-between align-items-start">
+        <div class="tg-panel-solution__text">
+          <div class="brand-primary-color f-size-lg-22 f-size-18 text-uppercase">
+            <?php echo $tg_software_solution_1_text['beforetitle'] ?>
+          </div>
+          <div class="f-size-lg-36 f-size-md-30 f-size-26 f-weight-700 l-height-110 py-10">
+            <?php echo $tg_software_solution_1_text['title'] ?>
+          </div>
+          <div class="f-size-lg-20 f-size-18">
+            <?php echo $tg_software_solution_1_text['subtitle'] ?>
+          </div>
+          <div class="f-size-lg-18 f-size-16 f-weight-300 pt-10 l-height-120">
+            <?php echo $tg_software_solution_1_text['description'] ?>
           </div>
         </div>
 
+        <div class="tg-panel-solution__jp">
+          <div class="tg-panel-solution__jp-img">
+            <img src="<?php echo esc_url($tg_software_solution_1_img['sizes']['info_block_one']) ?>"
+                 alt="">
+          </div>
+          <div class="tg-panel-solution__jp-layout"></div>
+        </div>
       </div>
     </div>
+
+    <div class="container pt-lg-40 pt-25">
+      <div class="row justify-content-center">
+        <div class="col-xl-10">
+
+          <div class="row">
+            <?php
+            if (!empty($tg_software_solution_1_blocks)) {
+              foreach ($tg_software_solution_1_blocks as $tg_software_block) { ?>
+                <div class="col-lg-6 py-15">
+                  <div class="tg-panel-solution__item p-md-30 p-20">
+                    <img src="<?php echo esc_url($tg_software_block['logo']['url']) ?>"
+                         alt="<?php echo esc_attr($tg_software_block['title']) ?>">
+
+                    <div class="f-size-lg-20 f-size-18 f-weight-500 l-height-120 mb-15">
+                      <?php echo esc_attr($tg_software_block['title']) ?>
+                    </div>
+
+                    <div class="tg-panel-solution__item-text">
+                      <?php echo $tg_software_block['text'] ?>
+                    </div>
+                  </div>
+                </div>
+              <?php }
+            } ?>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
   </div>
 
-</div>
+<?php
+$tg_software_solution_2_text = get_field('tg_software_solution_2_text');
+$tg_software_solution_2_img = get_field('tg_software_solution_2_img');
+$tg_software_solution_2_blocks = get_field('tg_software_solution_2_blocks');
+?>
+  <div class="tg-panel-solution tg-panel-solution_revers pb-lg-60 pb-35">
+
+    <div class="tg-panel-solution__top">
+      <div class="d-flex flex-wrap justify-content-between align-items-start">
+        <div class="tg-panel-solution__text">
+          <div class="brand-primary-color f-size-lg-22 f-size-18 text-uppercase">
+            <?php echo $tg_software_solution_2_text['beforetitle'] ?>
+          </div>
+          <div class="f-size-lg-36 f-size-md-30 f-size-26 f-weight-700 l-height-110 py-10">
+            <?php echo $tg_software_solution_2_text['title'] ?>
+          </div>
+          <div class="f-size-lg-20 f-size-18">
+            <?php echo $tg_software_solution_2_text['subtitle'] ?>
+          </div>
+          <div class="f-size-lg-18 f-size-16 f-weight-300 pt-10 l-height-120">
+            <?php echo $tg_software_solution_2_text['description'] ?>
+          </div>
+        </div>
+
+        <div class="tg-panel-solution__jp">
+          <div class="tg-panel-solution__jp-img">
+            <img src="<?php echo esc_url($tg_software_solution_2_img['sizes']['info_block_one']) ?>"
+                 alt="">
+          </div>
+          <div class="tg-panel-solution__jp-layout"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container pt-lg-40 pt-25">
+      <div class="row justify-content-center">
+        <div class="col-xl-10">
+
+          <div class="row">
+            <?php
+            if (!empty($tg_software_solution_2_blocks)) {
+              foreach ($tg_software_solution_2_blocks as $tg_software_block) { ?>
+                <div class="col-lg-6 py-15">
+                  <div class="tg-panel-solution__item p-md-30 p-20">
+                    <img src="<?php echo esc_url($tg_software_block['logo']['url']) ?>"
+                         alt="<?php echo esc_attr($tg_software_block['title']) ?>">
+
+                    <div class="f-size-lg-20 f-size-18 f-weight-500 l-height-120 mb-15">
+                      <?php echo esc_attr($tg_software_block['title']) ?>
+                    </div>
+
+                    <div class="tg-panel-solution__item-text">
+                      <?php echo $tg_software_block['text'] ?>
+                    </div>
+                  </div>
+                </div>
+              <?php }
+            } ?>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+<?php
+$tg_template_art_reverse = false;
+include(locate_template('template-parts/template-art.php', false, false));
+?>
 
 <?php get_footer(); ?>
---
+++ template-parts/template-art.php
@@ -0,0 +1,53 @@
+<?php
+
+$tg_template_art_link = !empty($tg_template_art_link) ? $tg_template_art_link : array(
+  'title' => 'Contact Us',
+  'url' => '#',
+  'target' => ''
+);
+
+$tg_template_art_title = !empty($tg_template_art_title) ? $tg_template_art_title : 'Visualize What’s Possible';
+$tg_template_art_description = !empty($tg_template_art_description) ? $tg_template_art_description : 'Schedule a demo and find out how Triage<br> Partners can help optimize your operations.';
+
+$tg_template_art_image = !empty($tg_template_art_image) ? $tg_template_art_image : array(
+  'sizes' => array(
+    'bg_page' => B_TEMP_URL . '/assets/img/iStock-1146417954.jpg'
+  )
+);
+
+$tg_template_art_reverse = $tg_template_art_reverse === true ? ' tg-template-art_reverse' : '';
+?>
+
+<div class="tg-template-art<?php echo $tg_template_art_reverse ?>">
+
+  <div class="tg-template-art__top">
+    <div class="container position-relative z-index-2">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-xl-11">
+
+          <div class="tg-template-art__title">
+            <div class="f-size-xl-32 f-size-lg-30 f-size-md-28 f-size-sm-26 f-size-24 text-uppercase f-weight-700 l-height-110 mb-20">
+              <?php echo $tg_template_art_title ?>
+            </div>
+            <div class="f-size-lg-20 f-size-md-18 f-size-16 f-weight-400 mb-40">
+              <?php echo $tg_template_art_description ?>
+            </div>
+
+            <?php
+            $target = ($tg_template_art_link['target'] === '_blank') ? 'target="_blank"' : '';
+            echo '<a href="' . $tg_template_art_link['url'] . '" class="tg-botton tg-botton_white" ' . $target . '>' . $tg_template_art_link['title'] . '</a>'
+            ?>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="tg-template-art__image">
+      <div class="tg-template-art__image-layer"
+           style="background-image:url(<?php echo esc_url($tg_template_art_image['sizes']['bg_page']) ?>);">
+      </div>
+    </div>
+  </div>
+
+</div>
