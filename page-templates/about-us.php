a37a33ed5f3f6bc3c8a2bfcd5d944f2e5b36c205
aerb20 <n0gep1990@gmail.com>
2020-05-28 17:30:45 +0300

    am about v1

---
+++ page-templates/about-us.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Template Name: About us
+ */
+
+// Do not allow directly accessing this file.
+if (!defined('ABSPATH')) {
+	exit('Direct script access denied.');
+}
+get_header();
+?>
+<div class="tg-about">
+	<div class="tg-about__approach pt-60 pb-15">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xl-11">
+					<div class="tg-title">OUR APPROACH</div>
+					<h3 class="tg-title tg-about__approach__tg-title-subtitle">Identify challenges, develop solutions.</h3>
+				</div>
+				<div class="tg-news__articles-container tg-maw-720 mb-40">
+					<div class="article">
+						<div class="image"
+						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
+						<div class="info px-15 py-5">
+							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
+							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
+						</div>
+					</div>
+					<div class="article">
+						<div class="image"
+						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
+						<div class="info px-15 py-5">
+							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
+							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
+						</div>
+					</div>
+					<div class="article">
+						<div class="image"
+						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
+						<div class="info px-15 py-5">
+							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
+							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
+						</div>
+					</div>
+					<div class="article">
+						<div class="image"
+						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
+						<div class="info px-15 py-5">
+							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
+							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="tg-about__story pt-60 pb-15">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xl-11">
+					<div class="tg-title">OUR STORY</div>
+				</div>
+				<div class="col-xl-10 py-30">
+					<div class="f-size-lg-18 f-size-md-16 f-size-15 f-weight-300">
+						<p>
+							Triage Partners was founded in 2003, when President and CEO Patricia Dominguez identified a need for innovative, technology-enabled solutions that addressed the unique operational challenges confronting the telecommunications industry—skilled resources, peak performance, logistics, mobility, and more. What started as a Tampa-based, IT staffing company serving primarily local customers and quickly evolved into a national managed services solutions provider that works with some of the biggest communications and technology companies in the world.
+						</p>
+						<p>
+							Since the start, we’ve always believed that growth and innovation play in the same sandbox. As our company grew, so did the industry’s need for more efficient workflows and better business intelligence. So we did what any forward-thinking company might do. We built it—a suite of proprietary software that empowers its users to streamline workflows, identify opportunities, and present information in a clear, understandable, and timely manner.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="tg-info-block-one pb-40">
+			<div class="d-flex flex-wrap justify-content-between">
+				<div class="tg-info-block-one__text">
+					<div class="f-size-lg-30 f-size-md-26 f-size-22 f-weight-700 l-height-110 mb-15 ">
+						We are proud to have earned recognition as one of the top woman-owned businesses in the nation, as well as one of Florida’s top 100 Diversity Companies.
+					</div>
+				</div>
+				<div class="tg-info-block-one__jp">
+					<div class="tg-info-block-one__jp-img">
+						<img src="<?php bloginfo('template_url'); ?>/assets/img/about.png" alt="">
+					</div>
+					<div class="tg-info-block-one__jp-layout"></div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xl-10 py-30">
+					<div class="f-size-lg-18 f-size-md-16 f-size-15 f-weight-300">
+						<p>
+							Today, we are a highly accomplished and hardworking team with expertise across logistics, software, design, and data. We take pride in our ability to supply the innovative tools, real-time data, and scalable resources that high-performing organizations need to be more efficient, productive, and profitable.
+						</p>
+						<p>
+							We are proud to have earned recognition as one of the top woman-owned businesses in the nation, as well as one of Florida’s top 100 Diversity Companies. Our founder Patricia was recognized by the Tampa Bay Business Journal as the Business Women of the Year in the Technology category.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
