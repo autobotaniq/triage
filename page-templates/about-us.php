32e6463a7be36b119eb5d100b7dd4de1149ef766
Denis Lipatov <ldu2601@gmail.com>
2020-05-29 17:35:16 +0300

    29.05.2020 17:35 add page Data We Enable

--- page-templates/about-us.php
+++ page-templates/about-us.php
@@ -5,195 +5,174 @@
 
 // Do not allow directly accessing this file.
 if (!defined('ABSPATH')) {
-	exit('Direct script access denied.');
+  exit('Direct script access denied.');
 }
 get_header();
 ?>
-<div class="tg-about">
-	<div class="tg-about__approach pt-60 pb-15">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-xl-11">
-					<div class="tg-title">OUR APPROACH</div>
-					<h3 class="tg-title tg-about__approach__tg-title-subtitle">Identify challenges, develop solutions.</h3>
-				</div>
-				<div class="tg-news__articles-container tg-maw-720 mb-40">
-					<div class="article">
-						<div class="image"
-						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
-						<div class="info px-15 py-5">
-							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
-							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
-						</div>
-					</div>
-					<div class="article">
-						<div class="image"
-						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
-						<div class="info px-15 py-5">
-							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
-							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
-						</div>
-					</div>
-					<div class="article">
-						<div class="image"
-						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
-						<div class="info px-15 py-5">
-							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
-							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
-						</div>
-					</div>
-					<div class="article">
-						<div class="image"
-						     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')"></div>
-						<div class="info px-15 py-5">
-							<h1 class="f-size-26 mb-0 py-10">We listen.</h1>
-							<p class="f-size-16 pb-5 mb-0">With our software and our expert resources, we can oversee all elements of your workflow—from labor productivity and organizational experience, to quality control and material tracking.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="tg-about__story pt-60 pb-15">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-xl-11">
-					<div class="tg-title">OUR STORY</div>
-				</div>
-				<div class="col-xl-10 py-30">
-					<div class="f-size-lg-18 f-size-md-16 f-size-15 f-weight-300">
-						<p>
-							Triage Partners was founded in 2003, when President and CEO Patricia Dominguez identified a need for innovative, technology-enabled solutions that addressed the unique operational challenges confronting the telecommunications industry—skilled resources, peak performance, logistics, mobility, and more. What started as a Tampa-based, IT staffing company serving primarily local customers and quickly evolved into a national managed services solutions provider that works with some of the biggest communications and technology companies in the world.
-						</p>
-						<p>
-							Since the start, we’ve always believed that growth and innovation play in the same sandbox. As our company grew, so did the industry’s need for more efficient workflows and better business intelligence. So we did what any forward-thinking company might do. We built it—a suite of proprietary software that empowers its users to streamline workflows, identify opportunities, and present information in a clear, understandable, and timely manner.
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="tg-info-block-one pb-40">
-			<div class="d-flex flex-wrap justify-content-between">
-				<div class="tg-info-block-one__text">
-					<div class="f-size-lg-30 f-size-md-26 f-size-22 f-weight-700 l-height-110 mb-15 ">
-						We are proud to have earned recognition as one of the top woman-owned businesses in the nation, as well as one of Florida’s top 100 Diversity Companies.
-					</div>
-				</div>
-				<div class="tg-info-block-one__jp">
-					<div class="tg-info-block-one__jp-img">
-						<img src="<?php bloginfo('template_url'); ?>/assets/img/about.png" alt="">
-					</div>
-					<div class="tg-info-block-one__jp-layout"></div>
-				</div>
-			</div>
-		</div>
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-xl-10 py-30">
-					<div class="f-size-lg-18 f-size-md-16 f-size-15 f-weight-300">
-						<p>
-							Today, we are a highly accomplished and hardworking team with expertise across logistics, software, design, and data. We take pride in our ability to supply the innovative tools, real-time data, and scalable resources that high-performing organizations need to be more efficient, productive, and profitable.
-						</p>
-						<p>
-							We are proud to have earned recognition as one of the top woman-owned businesses in the nation, as well as one of Florida’s top 100 Diversity Companies. Our founder Patricia was recognized by the Tampa Bay Business Journal as the Business Women of the Year in the Technology category.
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-    <div class="tg-ready pt-40 pb-20">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-11">
-                    <div class="row justify-content-between align-items-center">
-                        <div class="col-xl-8 col-lg-7 text-white pb-30 pb-lg-0">
-                            <div class="f-size-lg-32 f-size-md-28 f-size-24 text-uppercase f-weight-700 text-center">Ready to elevate your operations?</div>
-                            <div class="f-size-lg-20 f-size-md-18 f-size-16 text-center text-lg-left "><p><strong>Let’s connect.</strong> Learn more about our software, solutions, and services—and discover how to put them to work for your organization.</p>
-                            </div>
-                            <div class="text-center pt-lg-30 pt-20">
-                                <a href="//localhost:9000/" class="tg-botton tg-botton_white">Contact Us</a>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-lg-5 text-center">
-                            <img src="//localhost:9000/wp-content/uploads/2020/05/ready.png" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="tg-about__partners-container pt-30 pb-20">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-9">
-                    <div class="tg-title t-align-center t-transform-uppercase">Partners</div>
-                </div>
-                <div class="col-xl-9 py-30">
-                    <div class="f-size-lg-18 f-size-md-18 f-size-16 f-weight-300">
-                        <p>
-                            Triage Partners works with a variety of Fortune 100 companies to provide smart solutions and exceptional service to our clients. Get to know the companies we work with to help keep your business moving forward.
-                        </p>
-                    </div>
-                    <div class="partners">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/logos/att.png" alt="">
-                    </div>
-                </div>
+  <div class="tg-about">
+    <div class="tg-about__approach pt-60 pb-15">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-11">
+            <div class="tg-title">OUR APPROACH</div>
+            <h3 class="tg-title tg-about__approach__tg-title-subtitle">Identify challenges, develop solutions.</h3>
+          </div>
+          <div class="tg-news__articles-container tg-maw-720 mb-40">
 
-            </div>
-        </div>
-    </div>
-    <div class="tg-about__news-container pt-30 pb-20">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-9 block">
-                    <div class="tg-title tg-title__news t-transform-uppercase t-align-center">
-                        read TRIAGE partners in the news <br>
-                        <span class="tg-title__news__sub-title f-size-18 f-weight-400 t-transform-capitalize">View Our Case Studies</span>
+            <?php
+            $tg_about_approach_list = get_field('tg_about_approach_list');
+            if (!empty($tg_about_approach_list)) {
+              foreach ($tg_about_approach_list as $approach_list) { ?>
+                <div class="article">
+                  <div class="image"
+                       style="background-image: url(<?php echo $approach_list['img']['sizes']['tumb_partners'] ?>)"></div>
+                  <div class="info px-15 py-5">
+                    <div class="f-size-26 f-weight-700 py-10">
+                      <?php echo $approach_list['title'] ?>
                     </div>
-                    <div class="tg-about__news-container__articles flex-wrap">
-                        <div class="article mt-25 mx-5">
-                            <div class="image"
-                                 style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')">
-                            </div>
-                            <div class="info py-5 px-10">
-                                <h1 class="f-size-20 t-transform-uppercase pb-30">Title</h1>
-                                <a href="#" class="rd-more t-transform-uppercase f-weight-700">Read more</a>
-                            </div>
-                        </div>
-                        <div class="article mt-25 mx-5">
-                            <div class="image"
-                                 style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')">
-                            </div>
-                            <div class="info py-5 px-10">
-                                <h1 class="f-size-20 t-transform-uppercase pb-30">Title</h1>
-                                <a href="#" class="rd-more t-transform-uppercase f-weight-700">Read more</a>
-                            </div>
-                        </div>
-                        <div class="article mt-25 mx-5">
-                            <div class="image"
-                                 style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')">
-                            </div>
-                            <div class="info py-5 px-10">
-                                <h1 class="f-size-20 t-transform-uppercase pb-30">Title</h1>
-                                <a href="#" class="rd-more t-transform-uppercase f-weight-700">Read more</a>
-                            </div>
-                        </div>
-                    </div>
+                    <p class="f-size-16 pb-5 mb-0">
+                      <?php echo $approach_list['text'] ?>
+                    </p>
+                  </div>
                 </div>
-            </div>
+              <?php }
+            } ?>
+
+          </div>
         </div>
+      </div>
     </div>
-</div>
+
+    <?php
+    $tg_about_story_before_text = get_field('tg_about_story_before_text');
+    $tg_about_story_title = get_field('tg_about_story_title');
+    $tg_about_story_img = get_field('tg_about_story_img');
+    $tg_about_story_after_text = get_field('tg_about_story_after_text');
+    ?>
+    <div class="tg-about__story pt-60 pb-15">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-11">
+            <div class="tg-title">OUR STORY</div>
+          </div>
+          <div class="col-xl-10 py-30">
+            <div class="f-size-lg-18 f-size-md-16 f-size-15 f-weight-300">
+              <?php echo $tg_about_story_before_text ?>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tg-info-block-one pb-40">
+        <div class="d-flex flex-wrap justify-content-between">
+          <div class="tg-info-block-one__text">
+            <div class="f-size-lg-30 f-size-md-26 f-size-22 f-weight-700 l-height-110 mb-15 ">
+              <?php echo $tg_about_story_title ?>
+            </div>
+          </div>
+          <div class="tg-info-block-one__jp">
+            <div class="tg-info-block-one__jp-img">
+              <img src="<?php echo esc_url($tg_about_story_img['sizes']['bg_page']) ?>" alt="">
+            </div>
+            <div class="tg-info-block-one__jp-layout"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-10 py-30">
+            <div class="f-size-lg-18 f-size-md-16 f-size-15 f-weight-300">
+              <?php echo $tg_about_story_after_text ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    $tg_template_art_title = get_field('tg_story_art_title');
+    $tg_template_art_description = get_field('tg_story_art_description');
+    $tg_template_art_link = get_field('tg_story_art_link');
+    $tg_template_art_image = get_field('tg_story_art_image');
+
+    $tg_template_art_reverse = false;
+
+    include(locate_template('template-parts/template-art.php', false, false));
+    ?>
+
+    <?php
+    $tg_story_partners_description = get_field('tg_story_partners_description');
+    $tg_story_partners = get_field('tg_story_partners');
+    ?>
+    <div class="tg-about__partners-container pt-30 pb-20">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-9">
+            <div class="tg-title t-align-center t-transform-uppercase">Partners</div>
+          </div>
+          <div class="col-xl-9 py-30">
+            <div class="f-size-lg-18 f-size-md-18 f-size-16 f-weight-300">
+              <p>
+                <?php echo $tg_story_partners_description ?>
+              </p>
+            </div>
+            <div class="tg-about-partners">
+              <?php if (!empty($tg_story_partners)) {
+                foreach ($tg_story_partners as $partner) { ?>
+                  <div class="tg-about-partners__item"
+                       style="background-image:url(<?php echo esc_url($partner['url']) ?>);"></div>
+                <?php }
+              } ?>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <?php /* ?>
+    <div class="tg-about__news-container pt-30 pb-20">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 block">
+            <div class="tg-title tg-title__news t-transform-uppercase t-align-center">
+              read TRIAGE partners in the news <br>
+              <span class="tg-title__news__sub-title f-size-18 f-weight-400 t-transform-capitalize">View Our Case Studies</span>
+            </div>
+            <div class="tg-about__news-container__articles flex-wrap">
+              <div class="article mt-25 mx-5">
+                <div class="image"
+                     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')">
+                </div>
+                <div class="info py-5 px-10">
+                  <h1 class="f-size-20 t-transform-uppercase pb-30">Title</h1>
+                  <a href="#" class="rd-more t-transform-uppercase f-weight-700">Read more</a>
+                </div>
+              </div>
+              <div class="article mt-25 mx-5">
+                <div class="image"
+                     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')">
+                </div>
+                <div class="info py-5 px-10">
+                  <h1 class="f-size-20 t-transform-uppercase pb-30">Title</h1>
+                  <a href="#" class="rd-more t-transform-uppercase f-weight-700">Read more</a>
+                </div>
+              </div>
+              <div class="article mt-25 mx-5">
+                <div class="image"
+                     style="background-image: url('<?php bloginfo('template_url'); ?>/assets/img/news.jpg')">
+                </div>
+                <div class="info py-5 px-10">
+                  <h1 class="f-size-20 t-transform-uppercase pb-30">Title</h1>
+                  <a href="#" class="rd-more t-transform-uppercase f-weight-700">Read more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php */ ?>
+  </div>
 <?php get_footer(); ?>
---
+++ page-templates/data-we-enable.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Template Name: Data We Enable
+ */
+
+// Do not allow directly accessing this file.
+if (!defined('ABSPATH')) {
+  exit('Direct script access denied.');
+}
+
+get_header();
+
+?>
+
+
+<?php
+$tg_dwe_double_block = get_field('tg_dwe_double_block');
+?>
+  <div class="tg-double-block pt-lg-70 pt-40 pb-lg-50 pb-30">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-9 col-lg-11">
+
+          <div class="tg-double-block__container">
+            <div class="row">
+              <?php
+              if (!empty($tg_dwe_double_block)) {
+                foreach ($tg_dwe_double_block as $double_block) {
+                  ?>
+                  <div class="col-md-6 py-md-0 py-20">
+                    <div class="tg-double-block__item">
+                      <div class="f-size-lg-42 f-size-md-36 f-size-30 f-weight-700 l-height-110 mb-15">
+                        <?php echo $double_block['title'] ?>
+                      </div>
+
+                      <div class="tg-double-block__text">
+                        <?php echo $double_block['text'] ?>
+                      </div>
+                    </div>
+                  </div>
+                <?php }
+              } ?>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<?php
+$tg_dwe_partners_items = get_field('tg_dwe_partners_items');
+?>
+  <div class="tg-dwe-partners pt-25 pb-40">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-10">
+          <div class="tg-title text-uppercase text-white mb-25">
+            WHY TRIAGE partners?
+          </div>
+
+          <div class="row">
+            <?php if (!empty($tg_dwe_partners_items)) {
+              foreach ($tg_dwe_partners_items as $item) { ?>
+                <div class="col-lg-4 py-15">
+                  <div class="tg-dwe-partners__item">
+                    <div class="tg-dwe-partners__item-icon"
+                         style="background-image:url(<?php echo esc_url($item['icon']['url']) ?>);"></div>
+
+                    <div class="tg-dwe-partners__item-text">
+                      <div class="f-size-18 f-weight-700 mb-15">
+                        <?php echo $item['title'] ?>
+                      </div>
+
+                      <div class="f-size-16">
+                        <?php echo $item['text'] ?>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              <?php }
+            } ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<?php
+$tg_template_art_title = get_field('tg_dwe_art_title');
+$tg_template_art_description = get_field('tg_dwe_art_description');
+$tg_template_art_link = get_field('tg_dwe_art_link');
+$tg_template_art_image = get_field('tg_dwe_art_image');
+
+$tg_template_art_reverse = true;
+
+include(locate_template('template-parts/template-art.php', false, false));
+?>
+
+
+<?php get_footer(); ?>
